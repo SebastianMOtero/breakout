6a87cff632c1cbcc2c799a5643f9f403f7ade991
Sebastian <sebastian.otero@live.com.ar>
2020-04-16 20:07:05 -0300

    Finished Title Screen

---
+++ src/Dependencies.lua
@@ -0,0 +1,10 @@
+-- https://github.com/Ulydev/push
+push = require 'lib/push'
+
+-- https://github.com/vrld/hump/blob/master/class.lua
+Class = require 'lib/class'
+
+require 'src/constants'
+require 'src/StateMachine'
+require 'src/states/BaseState'
+require 'src/states/StartState'
